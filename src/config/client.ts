643e7bd88b595a5b328bc58f888c1875afa91d7f
Israel Schroeder Moreira <israelschroederm@gmail.com>
2025-01-30 10:05:54 -0300

    refator: started a new project

--- src/config/client.ts
+++
@@ -1,5 +0,0 @@
-const clientConfig = {
-  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '',
-};
-
-export default clientConfig;
--- src/config/server.ts
+++
@@ -1,3 +0,0 @@
-const serverConfig = {};
-
-export default serverConfig;
