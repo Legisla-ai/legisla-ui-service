bcf6caaf1e4ccdb132240a338f2e093e70e59a65
Israel Schroeder Moreira <israelschroederm@gmail.com>
2025-01-30 19:50:33 -0300

    install tailwind

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,25 @@
-import React from 'react';
-import Image from 'next/image';
-import styles from './Header.module.css';
+import type React from "react"
+import Image from "next/image"
+import { MessageCircle } from "lucide-react"
+import styles from "./Header.module.css"
+import { Button } from "@/components/ui/button"
 
 export const Header: React.FC = () => {
     return (
         <header className={styles.header}>
             <div className={styles.logoContainer}>
                 <div className={styles.logoIcon}>
-                    <Image
-                        src="images/icons/Favicon-04.svg"
-                        alt="Legisla.AI"
-                        width={32}
-                        height={32}
-                        className={styles.logo}
-                    />
+                    <Image src="/images/icons/Favicon-04.svg" alt="Legisla.AI" width={32} height={32} className={styles.logo} />
                 </div>
                 <span className={styles.logoText}>Legisla.AI</span>
             </div>
-            <div className={styles.avatarContainer}>
-                <Image
-                    src="https://avatar.iran.liara.run/public"
-                    alt="User Avatar"
-                    width={32}
-                    height={32}
-                    className={styles.avatar}
-                />
-            </div>
+            <Button variant="outline" className={styles.expertButton}>
+                <MessageCircle className="mr-2 h-4 w-4" />
+                Fale com um especialista
+            </Button>
         </header>
-    );
-};
+    )
+}
 
-export default Header;
+export default Header
+
